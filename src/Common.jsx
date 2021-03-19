1fe580cfbd508bf4f5301e246c3c6065a91ee2d3
vikrm <abhishek8587singh@gmail.com>
2021-03-19 10:13:30 +0530

    url fix

--- src/Common.jsx
+++ src/Common.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
 function Common(props){
-   let style = "col-8 text-justify textDiv h-100 d-flex align-items-center justify-content-center";
    return(
       <React.Fragment>
-       <div className="container row LandingPage w-100">
-         <div className={` col-8  ${style}`}>
+       <div className="container-fluid row w-100 h-100 ">
+         <div className="col-lg-8 col-md-8 col-sm-12 order-md-0 order-sm-1 order-1 common-resp">
           <div>
           <h1 >WELCOME TO {props.title} PAGE</h1>
           <h2>Keep visiting and rating us</h2>
           <h1 className="text-info">ABHISHEK ACODE</h1>
           </div>
          </div>
-         <div className="col-4 d-flex align-items-center">
+         <div className="col-lg-4 col-md-4 col-sm-12 order-sm-0 order-md-1 order-0 common-resp">
          <img src={props.imgsrc} alt="mainlogo" className="img-fluid" />
          </div>
        </div>
